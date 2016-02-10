47237c8305ace2e2431ce759992c21aaf6a1f8db
McBride36 <ddickins@iwu.edu>
2016-02-09 19:36:58 -0600

    fixed !check again

--- rollbot.py
+++ rollbot.py
@@ -237,7 +237,9 @@ class RollBot:
             ipaddress = ipaddress.replace("-", ".")
         else:
             return "Sorry, that's not an IP address!"
-        page = requests.get('http://check.getipintel.net/check.php?ip={}&contact={}'.format(ipaddress,"mcbride3692@gmail.com"))
+        with open('email.txt') as e:
+            email = e.read().strip()
+        page = requests.get('http://check.getipintel.net/check.php?ip={}&contact={}'.format(ipaddress, email))
         return "{}: chances of naughty IP = {}%".format(source, int(float(re.findall("(\d+(?:.\d+)?)", page.text)[0]) * 100))
 
     @command
